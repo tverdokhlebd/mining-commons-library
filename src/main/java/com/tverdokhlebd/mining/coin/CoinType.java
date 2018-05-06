bac7df8fe54731d084965e0190436069a64d11a3
Dmitry Tverdokhleb <tverdokhlebd@gmail.com>
2018-05-06 23:06:54 +0300

    Added coin type.

---
+++ src/main/java/com/tverdokhlebd/mining/coin/CoinType.java
@@ -0,0 +1,17 @@
+package com.tverdokhlebd.mining.coin;
+
+/**
+ * Enumerations of coin types.
+ *
+ * @author Dmitry Tverdokhleb
+ *
+ */
+public enum CoinType {
+
+    BTC,
+    ETH,
+    ETC,
+    XMR,
+    ZEC
+
+}
